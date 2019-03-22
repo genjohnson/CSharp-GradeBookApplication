167b6f6564405de7f78117bea8e49f87c94c797a
Genevieve Johnson <genevieve-johnson@uiowa.edu>
2019-03-22 15:11:18 -0500

    Create StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,9 @@
+﻿namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+        }
+    }
+}

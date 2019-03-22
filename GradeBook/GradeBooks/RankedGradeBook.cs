7754dda95c010858c08d52280bf8297094896609
Genevieve Johnson <genevieve-johnson@uiowa.edu>
2019-03-22 15:37:08 -0500

    Create GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,23 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count() < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students.");
+            }
+
+            return 'F';
         }
     }
 }
